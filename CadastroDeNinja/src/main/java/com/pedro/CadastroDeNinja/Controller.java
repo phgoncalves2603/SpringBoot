de9464f71828023304c805ff4ef575d9cc2d31bf
Pedro Pinto <phgoncalves2603@gmail.com>
2025-06-05 18:48:19 -0400

    Create First Route

---
+++ CadastroDeNinja/src/main/java/com/pedro/CadastroDeNinja/Controller.java
@@ -0,0 +1,16 @@
+package com.pedro.CadastroDeNinja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Cadastro de Ninja!";
+    }
+
+
+}
