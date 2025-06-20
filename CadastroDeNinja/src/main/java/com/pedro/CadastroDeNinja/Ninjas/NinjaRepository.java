2710d781767243995a74e442ccfbc288f2afa09b
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-06-19 23:32:45 -0400

    feat: set up H2  database

---
+++ CadastroDeNinja/src/main/java/com/pedro/CadastroDeNinja/Ninjas/NinjaRepository.java
@@ -0,0 +1,8 @@
+package com.pedro.CadastroDeNinja.Ninjas;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<ninjaModel, Long> {
+
+}
